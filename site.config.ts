a00832a19bee03d93470df0bbdc5f8f182d61e07
Aaron Bailey <aaron@aaronbailey.com>
2024-05-20 15:44:59 -0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '74b023457bcc4a2ba1b62d332de815c0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Your Pura Vida',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Your Pura Vida Members Portal',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
